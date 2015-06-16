4a14358d107ef0e36abed07f5dae6d863e4d88b1
Jorge Irazabal <jorge.irazabal@gmail.com>
2015-06-16 13:22:08 +0200

    Repository Creation

---
+++ server.R
@@ -0,0 +1,144 @@
+library(shiny)
+library(datasets)
+library(ggplot2)
+require(stats); require(graphics)
+
+shinyServer(function(input, output) {
+  
+  formulaText <- reactive({
+          if (input$type == "DriversKilled") {
+                  salida <- 'Drivers killed'
+          }
+          if (input$type == "Drivers") {
+                  salida <- 'Drivers Seriously Injured'
+          }
+          if (input$type == "front") {
+                  salida <- 'Front-seat passengers'
+          }
+          if (input$type == "rear") {
+                  salida <- 'Rear-seat passengers'
+          }
+          if (input$type == "VanKilled") {
+                  salida <- 'Van drivers'
+          }
+          if (input$type == "all") {
+                  salida <- 'All ocupant killed or seriously injured'
+          }
+          if (input$kms == "TRUE") {
+                  salida <- paste(salida,"<br/> Kilometers traveled: ")
+                  salida <- paste(salida,input$kmsInfluence)
+          }
+          if (input$petrol == "TRUE") {
+                  salida <- paste(salida,"<br/> Petrol Price: ")
+                  salida <- paste(salida,input$petrolInfluence)
+          }
+          salida
+  })
+  
+  
+  output$caption <- renderUI({
+    HTML(formulaText())
+  })
+  
+# Plot with the distribution of killed or seriously injured people  
+output$dataPlot <- renderPlot( {
+
+  df <- data.frame(Seatbelts)
+  # Obtain the year if the data
+  df$seq<- seq(1,192)
+  df$year<-1969 + floor((df$seq-1)/12)
+  # If kms has been selected, filter the data
+  if (input$kms == "TRUE") {
+    if (input$kmsInfluence == "<12000") {
+      df <- subset(df,kms<12000)
+    }
+    if (input$kmsInfluence == "12000-14000") {
+      df <- subset(df,kms>=12000 & kms<14000)
+    }
+    if (input$kmsInfluence == "14000-16000") {
+      df <- subset(df,kms>=14000 & kms<16000)
+    }
+    if (input$kmsInfluence == "16000-18000") {
+      df <- subset(df,kms>=16000 & kms<18000)
+    }
+    if (input$kmsInfluence == ">18000") {
+      df <- subset(df,kms>=18000)
+    }
+  }
+  # If petrol influence has been selected, filter the data
+  if (input$petrol == "TRUE") {
+    if (input$petrolInfluence == "<0.10") {
+      df <- subset(df,PetrolPrice<0.09)
+    }
+    if (input$petrolInfluence == "0.10-0.12") {
+      df <- subset(df,PetrolPrice>=0.09 & PetrolPrice<0.12)
+    }
+    if (input$petrolInfluence == ">0.12") {
+      df <- subset(df,PetrolPrice>=0.12)
+    }
+  }
+  # Load selected people type
+  if (input$type == "DriversKilled") {
+    finalData <- data.frame(df[,c(1,10)],'Drivers killed')
+    yLabel <- 'Drivers killed'
+  }
+  if (input$type == "Drivers") {
+    finalData <- data.frame(df[,c(2,10)],'Drivers Seriously Injured')
+    yLabel <- 'Drivers Seriously Injured'
+  }
+  if (input$type == "front") {
+    finalData <- data.frame(df[,c(3,10)],'Front-seat passengers')
+    yLabel <- 'Front-seat passengers'
+  }
+  if (input$type == "rear") {
+    finalData <- data.frame(df[,c(4,10)],'Rear-seat passengers')
+    yLabel <- 'Rear-seat passengers'
+  }
+  if (input$type == "VanKilled") {
+    finalData <- data.frame(df[,c(7,10)],'Van drivers')
+    yLabel <- 'Van drivers'
+  }
+  if (input$type == "all") {
+    finalData <- data.frame(df[,c(1,10)],'Drivers killed')
+    finalData <- setNames(finalData,c("Total","Year","Type"))
+    auxData <- data.frame(df[,c(2,10)],'Drivers Seriously Injured')
+    auxData <- setNames(auxData,c("Total","Year","Type"))
+    finalData <- rbind(finalData, auxData)
+    auxData <- data.frame(df[,c(3,10)],'Front-seat passengers')
+    auxData <- setNames(auxData,c("Total","Year","Type"))
+    finalData <- rbind(finalData, auxData)
+    auxData <- data.frame(df[,c(4,10)],'Rear-seat passengers')
+    auxData <- setNames(auxData,c("Total","Year","Type"))
+    finalData <- rbind(finalData, auxData)
+    auxData <- data.frame(df[,c(7,10)],'Van drivers')
+    auxData <- setNames(auxData,c("Total","Year","Type"))
+    finalData <- rbind(finalData, auxData)
+    yLabel <- 'All ocupant killed or seriously injured'
+  }
+  finalData <- setNames(finalData,c("Total","Year","Type"))
+  # Show Data
+  ggp <- ggplot(finalData, aes(x=Year, y=Total, fill=Type)) + 
+    geom_bar(stat="identity",se=FALSE) +
+    ylab(yLabel) +    
+    xlab("Year")   + 
+    geom_vline(xintercept=1982.45)
+
+  print(ggp)
+})
+
+output$prdPlot <- renderPlot({
+        # Obtain Data previous to 1983
+	prevData <- window(UKDriverDeaths, end = 1982+11/12)
+        # Draw this data
+	plot(prevData); 
+        # Predict data for then 24 next months
+	fit <- arima(prevData, c(1, 0, 0), seasonal = list(order = c(1, 0, 0)))
+	z <- predict(fit, n.ahead = 24)
+        # Add predict data to plot
+	ts.plot(UKDriverDeaths, z$pred,
+	        col = c("black", "red"),main="Comparation of real UK Driver Deaths and a prediction without compulsory wearing of seat belts", 
+	        xlab="Year", ylab="UK Driver Deaths by Month")
+
+  })
+
+})
---
+++ ui.R
@@ -0,0 +1,79 @@
+library(shiny)
+
+shinyUI(
+  navbarPage("Influence of seat belt use in road casualties",
+             tabPanel("Introduction",
+                      p("This is a study on the influence of the compulsory use of the seat belt in the number of road deaths or seriously injured"),
+                      hr(),
+                      h3("Instructions"),
+                      p("In the Exploratory Analysis tab can you see the number of occupants killed or seriously injured as if it is the driver, passenger types..., allowing to study the influence of the price of petrol or the traveled kilometers."),
+                      p("Comparation tab show the data of drivers killed o seriously injured, and a prediction and a prediction of what would have happened without the mandatory seat belt (be patient, it's slow)"),
+                      p("Data tab contain a reference to the data used")
+             ),
+             tabPanel("Exploratory Analysis",
+                      fluidPage(
+                        titlePanel("Road Casualties in Great Britain: 1969 - 1984"),
+                        sidebarLayout(
+                          sidebarPanel(                                                       
+                            radioButtons("type", "Driver or passenger killed or seriously injured :",
+                                         c("Drivers Killed" = "DriversKilled",
+                                           "Drivers Seriously Injured" = "Drivers",
+                                           "Front-seat passengers killed or seriously injured" = "front",
+                                           "Rear-seat passengers killed or seriously injured" = "rear",
+                                           "Van drivers killed or seriously injured" = "VanKilled",
+                                           "All killed or seriously injured" = "all"),selected="all"),                                           
+                            checkboxInput("kms", "Distance driven influence"),
+                            conditionalPanel(
+                              condition = "input.kms == true",
+                              selectInput("kmsInfluence", "Distance driven",
+                                          list("<12000", "12000-14000", "14000-16000", "16000-18000", ">18000"))
+                            ),
+                            checkboxInput("petrol", "Petrol price influence"),
+                            conditionalPanel(
+                              condition = "input.petrol == true",
+                              selectInput("petrolInfluence", "Petrol Price",
+                                          list("<0.10","0.10-0.12",">0.12"))
+                            )
+                            
+                          ),
+                          
+                          mainPanel(
+                            h3(htmlOutput("caption")),
+                            plotOutput("dataPlot")
+                            )
+                          )
+                        )
+             ),
+             tabPanel("Comparation",
+                      p("This is a comparation between the real data (in black) and a prediction if it was not compulsory to use seat belts (in red)"),
+                      plotOutput("prdPlot")
+             ),
+             tabPanel("Data",
+                      h2("Road Casualties in Great Britain 1969–84"),
+                      hr(),
+                      h3("Description"),
+                      helpText("It is a time series giving the monthly totals ",
+                               "of car drivers in Great Britain killed or ",
+                               "seriously injured Jan 1969 to Dec 1984. ",
+                               "Compulsory wearing of seat belts was introduced",
+                               " on 31 Jan 1983."),
+                      h3("Data Format"),
+                      p("DriversKilled: Car drivers killed"),
+                      p("drivers:       Drivers killed or seriously injured"),
+                      p("front:         front-seat passengers killed or seriously injure"),
+                      p("rear:          rear-seat passengers killed or seriously injured"),
+                      p("kms:           Distance driven"),
+                      p("PetrolPrice:   petrol price"),
+                      p("VanKilled:     number of van ('light goods vehicle') drivers"),
+                      p("law:           0/1: was the law in effect that month?"),
+                      
+                      h3("Source"),                      
+                      p("Harvey, A.C. (1989) Forecasting, Structural Time Series Models and the Kalman Filter. Cambridge University Press, pp. 519Ã¢ÂÂ523"),
+                      p("Durbin, J. and Koopman, S. J. (2001) Time Series Analysis by State Space Methods. Oxford University Press. http://www.ssfpack.com/dkbook/"),
+                      h3("References"),                      
+                      p("Harvey, A.C. (1989) Forecasting, Structural Time Series Models and the Kalman Filter. Cambridge University Press, pp. 519Ã¢ÂÂ523"),
+                      p("Durbin, J. and Koopman, S. J. (2001) Time Series Analysis by State Space Methods. Oxford University Press. http://www.ssfpack.com/dkbook/"),
+                      textOutput('sumData')
+             )
+  )
+)
